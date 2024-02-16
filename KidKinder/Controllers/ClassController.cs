04898b0cf32ec5c39ecb0f705324ed875defa342
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-02-16 12:50:58 +0100

    .

---
+++ KidKinder/Controllers/ClassController.cs
@@ -0,0 +1,43 @@
+﻿using KidKinder.Context;
+using KidKinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KidKinder.Controllers
+{
+    public class ClassController : Controller
+    {
+        KidKinderContext c = new KidKinderContext();
+        public ActionResult ClassList()
+        {
+            var values = c.ClassRooms.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult EditClass(int id)
+        {
+            var values = c.ClassRooms.Find(id);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult EditClass(ClassRoom p)
+        {
+            var value = c.ClassRooms.Find(p.ClassRoomId);
+            value.Title = p.Title;
+            value.Description = p.Description;
+            value.AgeofKids = p.AgeofKids;
+            value.TotalSeats = p.TotalSeats;
+            value.ClassTime = p.ClassTime;
+            value.Price = p.Price;
+            value.ImageUrl = p.ImageUrl;
+            c.SaveChanges();
+            return RedirectToAction("ClassList");
+        }
+
+
+    }
+}
