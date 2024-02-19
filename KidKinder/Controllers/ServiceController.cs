8cd031d2c967216c7b914786b8e2e79a17fb0730
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-02-19 12:13:08 +0100

    .

---
+++ KidKinder/Controllers/ServiceController.cs
@@ -0,0 +1,60 @@
+﻿using KidKinder.Context;
+using KidKinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KidKinder.Controllers
+{
+    public class ServiceController : Controller
+    {
+        KidKinderContext c = new KidKinderContext();
+        public ActionResult ServiceList()
+        {
+            var values = c.Services.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddService()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddService(Service p)
+        {
+            c.Services.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("ServiceList");
+        }
+
+
+        public ActionResult DeleteService(int id)
+        {
+            c.Services.Remove(c.Services.Find(id));
+            c.SaveChanges();
+            return RedirectToAction("ServiceList");
+
+        }
+
+        [HttpGet]
+        public ActionResult EditService(int id)
+        {
+            var values = c.Services.Find(id);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult EditService(Service p)
+        {
+            var value = c.Services.Find(p.ServiceId);
+            value.Title = p.Title;
+            value.Description = p.Description;
+            value.IconUrl = p.IconUrl;
+            c.SaveChanges();
+            return RedirectToAction("ServiceList");
+        }
+
+    }
+}
--- KidKinder/Controllers/TeacherController.cs
+++ KidKinder/Controllers/TeacherController.cs
@@ -1,4 +1,5 @@
 ﻿using KidKinder.Context;
+using KidKinder.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,50 @@ namespace KidKinder.Controllers
 {
     public class TeacherController : Controller
     {
-        KidKinderContext context = new KidKinderContext();
+        KidKinderContext c = new KidKinderContext();
         public ActionResult TeacherList()
         {
-            var values = context.Teachers.ToList();
+            var values = c.Teachers.ToList();
             return View(values);
         }
+
+        [HttpGet]
+        public ActionResult AddTeacher()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddTeacher(Teacher p)
+        {
+            c.Teachers.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("TeacherList");
+        }
+
+        public ActionResult DeleteTeacher(int id)
+        {
+            c.Teachers.Remove(c.Teachers.Find(id));
+            c.SaveChanges();
+            return RedirectToAction("TeacherList");
+
+
+        }
+        [HttpGet]
+        public ActionResult EditTeacher(int id)
+        {
+            var values = c.Teachers.Find(id);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult EditTeacher(Teacher p)
+        {
+            var value = c.Teachers.Find(p.TeacherId);
+            value.Title = p.Title;
+            value.NameSurname = p.NameSurname;
+            value.ImageUrl = p.ImageUrl;
+            c.SaveChanges();
+            return RedirectToAction("TeacherList");
+        }
     }
+  
 }
